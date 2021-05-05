d741c443235daf133c48238e019e380e12b26c7d
Cristian Flores <cristianflores.ee@gmail.com>
2021-05-05 02:29:57 -0400

    🚸 (products) delete cart label

--- src/app/core/services/api/products/products.service.ts
+++ src/app/core/services/api/products/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { IProduct } from "@pages/products/models/product.model";
+import { IProduct } from "@pages/products/models";
 
 @Injectable({
   providedIn: "root",
@@ -8,43 +8,57 @@ export class ProductsService {
   products: IProduct[] = [
     {
       id: "1",
-      image: "assets/images/food/fruit1.png",
-      title: "Camiseta",
+      image: "assets/images/food/vegetable1.png",
+      title: "Repollo",
       price: 80000,
       description: "bla bla bla bla bla",
     },
     {
       id: "2",
-      image: "assets/images/food/fruit2.png",
-      title: "Hoodie",
+      image: "assets/images/food/vegetable2.png",
+      title: "Brócoli",
       price: 80000,
       description: "bla bla bla bla bla",
     },
     {
       id: "3",
-      image: "assets/images/mug.png",
-      title: "Mug",
+      image: "assets/images/food/vegetable3.png",
+      title: "Zanahoria",
       price: 80000,
       description: "bla bla bla bla bla",
     },
     {
       id: "4",
-      image: "assets/images/pin.png",
-      title: "Pin",
+      image: "assets/images/food/vegetable4.png",
+      title: "Remolacha",
       price: 80000,
       description: "bla bla bla bla bla",
     },
     {
       id: "5",
-      image: "assets/images/stickers1.png",
-      title: "Stickers",
+      image: "assets/images/food/vegetable5.png",
+      title: "Rábano",
       price: 80000,
       description: "bla bla bla bla bla",
     },
     {
       id: "6",
-      image: "assets/images/stickers2.png",
-      title: "Stickers",
+      image: "assets/images/food/fruit1.png",
+      title: "Uva",
+      price: 80000,
+      description: "bla bla bla bla bla",
+    },
+    {
+      id: "7",
+      image: "assets/images/food/fruit2.png",
+      title: "Plátano",
+      price: 80000,
+      description: "bla bla bla bla bla",
+    },
+    {
+      id: "8",
+      image: "assets/images/food/fruit3.png",
+      title: "Manzana",
       price: 80000,
       description: "bla bla bla bla bla",
     },
